1d9ec314824b7ff36f1c8e93175460eed85403a1
Satyabrata Ghosh <satyabrataghosh996@gmail.com>
2025-11-20 13:56:41 +0530

    import all files inside main.dart

---
+++ farmer_app/lib/main.dart
@@ -0,0 +1,27 @@
+// lib/main.dart
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import 'routes.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Agriculture App',
+      theme: ThemeData(primarySwatch: Colors.green, useMaterial3: true),
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/',
+      routes: appRoutes,
+    );
+  }
+}
